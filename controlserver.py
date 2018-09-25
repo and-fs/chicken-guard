9196cbe1c08338420efd4d7657224f0ca93a9ad0
Andre Fritzsche-Schwalbe <afs@Andres-MBP.fritz.box>
2018-09-25 19:14:13 +0200

    Fixed syntax error

--- controlserver.py
+++ controlserver.py
@@ -143,9 +143,6 @@ class Controller(LoggableClass):
         self.job_timer.Terminate()
         self.job_timer.join()
         self.job_timer = None
-
-    def _CheckDoorState(self):
-
 # ------------------------------------------------------------------------
 class DataServer(socketserver.ThreadingMixIn, xmlrpc.server.SimpleXMLRPCServer):
     """
