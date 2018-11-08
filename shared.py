956c7c9b148014d9d8c9715e8232f984ccd9bce7
André <andre.fritzsche@web.de>
2018-11-08 07:51:32 +0100

    Config dokumentiert

--- shared.py
+++ shared.py
@@ -116,38 +116,116 @@ class LoggableClass:
         """
         if hasattr(self.logger, name):
             return getattr(self.logger, name)
-        return Config.Get(name, match_exact = True)
+        return Config.Get(name, case_sensitive = True)
         #raise AttributeError("Instance of %s has no attribute '%s'" % (self.__class__, name))
 # --------------------------------------------------------------------------------------------------
 class _NOTSET:
     pass
 # --------------------------------------------------------------------------------------------------
 class Config:
+    """
+    Singleton-Klasse für den zentralen Zugriff auf die veränderbare Konfiguration.
 
+    Lädt alle Elemente aus :mod:`config` und stellt diese hier über :meth:`Get` zur Verfügung.
+    Änderungen am Inhalte von :mod:`config` können via :meth:`Update` neu eingeladen werden
+    und stehen dann beim nächsten Zugriff mittels :meth:`Get`zur Verfügung.
+    Groß-/Kleinschreibung wird dabei ignoriert, solange der jeweilige Parameter ``case_sensitive``
+    nicht mit ``True`` evaluiert.
+
+    .. code-block:: python
+
+        >>> Config.Get("SUNRISE_INTERVAL")
+        7000
+        >>> Config.Get("sunrise_interval")
+        7000
+        >>> Config.Set("SUNRISE_INTERVAL", 3500) # temporär ändern
+        >>> Config.Get("sunrise_interval")
+        3500
+        >>> Config.Update() # aus Datei nachladen
+        >>> Config.Get("SUNRISE_INTERVAL")
+        7000
+    """
+
+    # pylint: disable=W0212
+
+    #: Dieser Class-Member hält die Singleton-Instanz.
     _instance = None
 
     @classmethod
-    def Get(cls, name, default = _NOTSET, match_exact = False):
+    def Get(cls, name:str, default = _NOTSET, case_sensitive:bool = False):
+        """
+        Gibt einen benannten Konfigurationsparameter zurück.
+
+        :param str name: Name des Parameters.
+
+        :param any default: Standardwert der zurückgeliefert werden soll,
+            wenn ``name`` nicht gefunden wurde.
+
+        :param bool case_sensitive: Gibt an, ob die Groß-/Kleinschreibung beim Finden des
+            Konfigurationswertes für ``name`` beachtet werden soll.
+
+        :returns: Den gefundenen Konfigurationswert oder den optional angegebenen Standardwert.
+
+        :raises AttributeError: Falls der Konfigurationswert nicht gefunden wurde UND kein
+            Standardwert angegeben ist.
+        """
         cfg = cls.GetInstance()
-        return cfg._Get(name, default, match_exact = match_exact)
+        return cfg._Get(name, default, case_sensitive = case_sensitive)
 
     @classmethod
-    def Set(cls, name, value, do_update = True):
+    def Set(cls, name: str, value, do_update:bool = True):
+        """
+        Setzt den Wert des Konfigurationsparameters ``name`` auf ``value``.
+
+        :param bool do_update: Wenn ``True`` werden etwaige Update-Handler benachrichtigt.
+            Siehe dazu auch :meth:`RegisterUpdateHandler`.
+
+        .. seealso::
+
+            :meth:`Get`
+            :meth:`RegisterUpdateHandler`
+            :meth:`Update`
+
+        .. warning::
+
+            Die Werte werden nicht dauerhaft gesetzt, spätestens nach dem nächsten
+            :meth:`Update` oder Neustart ist der Wert wieder auf dem Ursprung.
+        """
         cfg = cls.GetInstance()
         cfg._Set(name, value, do_update = do_update)
 
     @classmethod
     def Update(cls):
+        """
+        Lädt die Konfiguration aus :mod:`config` neu nach und ruft dann die installierten
+        Update-Handler auf.
+
+        .. seealso::
+
+            :meth:`RegisterUpdateHandler`
+        """
         cfg = cls.GetInstance()
         cfg._Update()
 
     @classmethod
     def RegisterUpdateHandler(cls, hdl):
+        """
+        Registriert einen Handler ``hdl`` der ohne Parameter gerufen wird, wenn die Konfiguration
+        aktualisiert wurde.
+
+        .. seealso::
+
+            :meth:`Update`
+            :meth:`Set`
+        """
         cfg = cls.GetInstance()
         cfg._RegisterUpdateHandler(hdl)
 
     @classmethod
     def GetInstance(cls):
+        """
+        Gibt die Singleton-Instanz dieser Klasse zurück.
+        """
         if cls._instance is None:
             cls._instance = cls()
         return cls._instance
